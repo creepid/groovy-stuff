f9f851cf8ffa908662012e0222e9060bb4dc60db
creepid <mikhail.complete@gmail.com>
2015-12-02 17:26:43 +0300

    closure add

--- size-filter-closure.groovy
+++ size-filter-closure.groovy
@@ -13,9 +13,6 @@ Closure sizeUpTo6 = filter6.&sizeUpTo
 
 def words = ['long string', 'medium', 'short', 'tiny']
 
-println  words.find (sizeUpTo6)
-println  words.find (filter5.&sizeUpTo)
-
 assert 'medium' == words.find (sizeUpTo6)
 assert 'short' == words.find (filter5.&sizeUpTo)
 
